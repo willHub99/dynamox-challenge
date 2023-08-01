94f774b53c557c18cb83172f99e83cfb0f23203b
Willian Rodrigo Huber <willianhuber99@gmail.com>
2023-07-31 22:02:48 -0300

    create project, add animation files, change color theme and create main packages

---
+++ app/src/main/java/com/example/dynamoxchallenge/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.dynamoxchallenge.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val ButtercupColor = Color(0xFFF3B811)
+val DuneColor = Color(0xFF2F2D2B)
+val WestarColor = Color(0xFFDDDACF)
+val CornHarvestColor = Color(0xFF8C640C)
+val SuccessColor = Color(0xFF24FF00)
+val ErrorColor = Color(0xFFF50606 )
